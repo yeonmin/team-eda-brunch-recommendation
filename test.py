1be1db9a709cce96dcc0a671dbebe110b631f7f4
김태진 <jin@gimtaejin-ui-MacBookPro.local>
2019-07-23 23:48:12 +0900

    Add Function # utils.py / check_recs_duplication Check that how many times recs are duplicated in file

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import utils
 
 # get entropy score
-print(utils.get_entropy_from_file('recommend.txt', False))
+utils.get_entropy_from_file('recommend.txt', False)
+
+# Compare recommend files
+utils.compare_results('recommend.txt', 'recommend_2.txt', False)
+
+# Check recs duplication
+utils.check_recs_duplication('recommend.txt', False)
--- utils.py
+++ utils.py
@@ -25,13 +25,18 @@ def get_entropy_from_file(f_name, isTest):
             rec = line[1:]
             id_to_recs[uid] = rec
 
-    return _entropy_diversity(id_to_recs, topn)
+    print("Entropy from file({}) : {}".format(f_name, _entropy_diversity(id_to_recs, topn)))
+
+
 
 def compare_results(f_name_1, f_name_2, isTest):
     sz = 3000
     if isTest == True :
         sz = 5000
     
+    wrong_cnt = 0
+    intersection_cnt = 0
+
     with open(f_name_1, 'r') as f1 :
         with open(f_name_2, 'r') as f2 :
             for i in range(sz) :
@@ -39,8 +44,32 @@ def compare_results(f_name_1, f_name_2, isTest):
                 recs_2 = f2.readline().strip().split(' ')[1: ]
 
                 compare = [(i, recs_1[i], recs_2[i]) for i in range(100) if recs_1[i] != recs_2[i]]
-                print(compare)
-                
+                # print(compare)
+                wrong_cnt += len(compare)
+                intersection_cnt += len(set(recs_1) & set(recs_2))
+
                 # (optional)
-                if recs_1 != recs_2 :
-                    raise
+                # if recs_1 != recs_2 :
+                #     raise
+    print("Wrong count : ", wrong_cnt)
+    print("Duplicated count : ", intersection_cnt)
+
+def check_recs_duplication(f_name, isTest):
+    recs_dup_cnt = 0
+    dup_uid = []
+    sz = 3000
+    if isTest == True :
+        sz = 5000
+
+    with open(f_name, 'r') as f :
+        for i in range(sz):
+            line = f.readline().strip().split(' ')
+            uid = line[0]
+            rec = line[1:]
+
+            if len(set(rec)) != 100 :
+                recs_dup_cnt += 1
+                dup_uid.append(uid)
+
+    print('Recs Duplication count : ', recs_dup_cnt)
+    print('Duplicated Uids : ', dup_uid)
